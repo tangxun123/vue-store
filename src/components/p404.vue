f2175af235c06e6e54c1692da9fc9c1afd7dc1e2
tangxun <tangxun@dataexa.com>
2020-05-06 14:38:24 +0800

    添加４０４页面

---
+++ src/components/p404.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="p404">
+      <h1>page notFind</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'notFind'
+}
+</script>
+
+<style scoped lang="scss">
+  .p404{
+    width: 120px;
+    margin: 0 auto;
+    p{
+      line-height: 300px;
+    }
+  }
+</style>
